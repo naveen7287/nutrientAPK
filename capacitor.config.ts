14fb4cf10405efa5d63c550f1be9bfbb39171bed
Naveen <padilamnaveen@gmail.com>
2026-03-16 19:15:06 +0530

    update project before pull

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nutriscan.app',
+  appName: ' NutriScan AI',
+  webDir: 'dist'
+};
+
+export default config;
